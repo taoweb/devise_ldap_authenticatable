a765cc3ef2c29ac6ea7619a4b68fbc61b4eca3c3
Cássio Marques <cassiommc@gmail.com>
2011-02-04 10:54:04 -0200

    Tries to bind either through openldap or ad

--- lib/devise_ldap_authenticatable/ldap_adapter.rb
+++ lib/devise_ldap_authenticatable/ldap_adapter.rb
@@ -6,13 +6,30 @@ module Devise
   # ::Devise.ldap_host
   module LdapAdapter
     def self.valid_credentials?(login, attributes, password)
+      active_directory_binding || openldap_binding
+    end
+
+    private
+    def active_directory_binding
       login = "#{login}@#{::Devise.ldap_base_dn.gsub(/(.+,dc=)/, '')}"
       @encryption = ::Devise.ldap_ssl ? :simple_tls : nil
       ldap = Net::LDAP.new(:encryption => @encryption, :base => ::Devise.ldap_base_dn)
       ldap.host = ::Devise.ldap_host
       ldap.port = ::Devise.ldap_port
       ldap.auth login, password
-      ldap.bind ? true : false
+      ldap.bind
+    end
+
+    def openldap_binding
+      login = "#{::Devise.ldap_login_attribute}=#{login},"
+      login += "#{attributes}," unless attributes.nil?
+      login += ::Devise.ldap_base_dn
+      @encryption = ::Devise.ldap_ssl ? :simple_tls : nil
+      ldap = Net::LDAP.new(:encryption => @encryption)
+      ldap.host = ::Devise.ldap_host
+      ldap.port = ::Devise.ldap_port
+      ldap.auth login, password
+      ldap.bind
     end
   end
 end
