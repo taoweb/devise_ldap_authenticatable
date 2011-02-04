63b3a0338783990a14425d8e1500565e11b585dc
Cássio Marques <cassiommc@gmail.com>
2011-02-04 09:14:54 -0200

    Making changes specific to work with ActiveDirectory

--- lib/devise_ldap_authenticatable/ldap_adapter.rb
+++ lib/devise_ldap_authenticatable/ldap_adapter.rb
@@ -5,23 +5,14 @@ module Devise
   # simple adapter for ldap credential checking
   # ::Devise.ldap_host
   module LdapAdapter
-
     def self.valid_credentials?(login, attributes, password)
-      login = "#{::Devise.ldap_login_attribute}=#{login},"
-      login += "#{attributes}," unless attributes.nil?
-      login += ::Devise.ldap_base_dn
+      login = "#{login}@#{::Devise.ldap_base_dn.gsub(/(.+,dc=)/, '')}"
       @encryption = ::Devise.ldap_ssl ? :simple_tls : nil
-      ldap = Net::LDAP.new(:encryption => @encryption)
+      ldap = Net::LDAP.new(:encryption => @encryption, :base => ::Devise.ldap_base_dn)
       ldap.host = ::Devise.ldap_host
       ldap.port = ::Devise.ldap_port
       ldap.auth login, password
-      if ldap.bind
-        true
-      else
-        false
-      end
+      ldap.bind ? true : false
     end
-
   end
-
 end
